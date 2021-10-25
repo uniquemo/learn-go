f588f03b5ceba7eff0a2ad06abd907d56926efc9
unique.mo <myx3901051@qq.com>
2021-10-26 07:41:50 +0800

    feat: add 类型

---
+++ go_learning/src/ch3/type_test/type_test.go
@@ -0,0 +1,28 @@
+package type_test
+
+import "testing"
+
+type MyInt int64
+
+func TestType(t *testing.T) {
+	var a int32 = 1
+	var b int64
+	b = int64(a)
+	var c MyInt
+	c = MyInt(b)
+	t.Log(a, b, c)
+}
+
+func TestPoint(t *testing.T) {
+	a := 1
+	aPtr := &a
+	// aPtr = aPtr + 1	// Go不支持指针运算
+	t.Log(a, aPtr)
+	t.Logf("%T %T", a, aPtr)
+}
+
+func TestString(t *testing.T) {
+	var s string
+	t.Log("*" + s + "*")
+	t.Log(len(s))
+}
